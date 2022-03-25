e3e0a52e0147a6aea5f7f79c7a64b623eac43fd9
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-03-24 21:36:00 -1000

    fixed path to logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.env']
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_logo = "build/html/_images/hot_logo.jpg"
+html_logo = "build/html/_images/logo_HOT.jpg"
 html_title = "WHOTS"
 
 # Add any paths that contain custom static files (such as style sheets) here,
