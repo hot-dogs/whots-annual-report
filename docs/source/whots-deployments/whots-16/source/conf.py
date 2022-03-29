e1aa7fc2c1c1f43919e0554da4bc82d9e6bcbc16
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-03-29 06:36:25 -1000

    html_logo path

--- docs/source/whots-deployments/whots-16/source/conf.py
+++ docs/source/whots-deployments/whots-16/source/conf.py
@@ -75,7 +75,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.env']
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_logo = "../whots-16/source/_static/_images/logo_HOT.jpg"
+html_logo = "source/_static/_images/logo_HOT.jpg"
 html_title = "WHOTS"
 
 # Add any paths that contain custom static files (such as style sheets) here,
