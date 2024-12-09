5843200d1661521f97e345bf3a41adde9b7ccadd
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2024-12-09 13:13:44 -1000

    removed latex templates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
+from datetime import datetime
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 
-project = 'whots-annual-report'
-copyright = '2024, Fernando Carvalho Pacheco'
-author = 'Fernando Carvalho Pacheco'
+project = "whots-annual-report"
+copyright = f"{datetime.now().year}, Hawaii Ocean Time-series (HOT)"
+author = "Hawaii Ocean Time-series (HOT)"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = "1.0.0"
 
 extensions = [
     "myst_parser",
@@ -32,13 +33,18 @@ extensions = [
     "nbsphinx",
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosectionlabel",
+    "sphinxcontrib.bibtex",
 ]
 
-intersphinx_mapping = {
-    "whots-16": ("https://whots-annual-report.readthedocs.io/projects/whots16-data-report/en/latest/", None),
-}
 
-myst_url_schemes = ["http", "https", ]
+bibtex_bibfiles = ["latex_templates/refs.bib"]
+bibtex_default_style = "unsrt"
+bibtex_reference_style = "author_year"
+
+myst_url_schemes = [
+    "http",
+    "https",
+]
 
 # Added cross reference for headings
 myst_heading_anchors = 3
@@ -56,28 +62,39 @@ autosectionlabel_prefix_document = True
 autosectionlabel_maxdepth = 3
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-source_suffix = '.md'
+templates_path = ["_templates"]
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 # master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.env']
+exclude_patterns = [
+    "_build",
+    "build",
+    "thumbs.db",
+    ".ds_store",
+    ".env",
+    ".rst",
+    "Thumbs.db",
+    ".DS_store",
+]
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_logo = "../source/_static/_images/logo_HOT.png"
+html_logo = "../source/_static/_images/new_logo_HOT.png"
 html_title = "WHOTS"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_theme_options = {
     "repository_url": "https://github.com/hot-dogs/whots-annual-report",
@@ -85,4 +102,3 @@ html_theme_options = {
     "use_issues_button": True,
     "home_page_in_toc": False,
 }
-
